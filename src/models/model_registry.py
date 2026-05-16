78a0e98a365de91025463c6c476fd59da3835374
Aishwarya Vadlamudi <aish.vadlamudi@gmail.com>
2026-05-16 16:31:31 -0700

    Developed model architecture for the neural network

---
+++ src/models/model_registry.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+from typing import List
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_features: int, hidden_units: List, output_features: int):
+        super().__init__()
+        all_layers = []
+        for hidden_unit in hidden_units:
+            layer = nn.Linear(input_features, hidden_unit)
+            all_layers.append(layer)
+            all_layers.append(nn.ReLU())
+            input_features = hidden_unit
+        output_layer = nn.Linear(hidden_units[-1], output_features)
+        all_layers.append(output_layer)
+        self.model = nn.Sequential(*all_layers)
+
+    def forward(self, X):
+        output = self.model(X)
+        return output
---
+++ tests/test_model_registry.py
@@ -0,0 +1,16 @@
+from src.models.model_registry import NeuralNet
+import torch
+
+def test_model_registry():
+    input_features = 2
+    hidden_units = [6, 3]
+    output_features = 1
+
+    model = NeuralNet(input_features, hidden_units, output_features)
+    print(model)
+    X = torch.randn(32, 2)
+
+    output = model(X)
+    print(f"Output shape: {output.shape}")
+
+    assert X.shape[0] == output.shape[0], f"Input and output row count don't match"
