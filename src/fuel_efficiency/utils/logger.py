df90b228555a6cdaa07581acf7897535acbbad58
Aishwarya Vadlamudi <aish.vadlamudi@gmail.com>
2026-05-15 21:20:56 -0700

    Initial EDA and added logging and exception scripts

---
+++ src/fuel_efficiency/utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+from pathlib import Path
+from datetime import datetime
+
+# Generate a unique log file name based on current timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = Path.cwd() / "logs" / LOG_FILE
+
+
+LOG_FILE_PATH = logs_path / LOG_FILE
+
+logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
