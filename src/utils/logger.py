90d2073fdfdc91a6afc931861ad26a143e1d4baf
Aishwarya Vadlamudi <aish.vadlamudi@gmail.com>
2026-05-21 14:17:13 -0700

    bug fix logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,6 +8,9 @@ import logging
 from pathlib import Path
 from datetime import datetime
 
+LOG_DIR = Path.cwd() / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+
 LOG_FILE = LOG_DIR / f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logger = logging.getLogger("fuel_efficiency")
